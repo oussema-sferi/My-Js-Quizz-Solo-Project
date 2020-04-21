99d723690b59bf0eafad6722872357c467cabb84
oussema-sferi <oussema.sferi@gmail.com>
2020-04-21 02:17:51 +0100

    need more improvement

--- js/plugins.js
+++ js/plugins.js
@@ -4,7 +4,7 @@ var myWelcomeText = "This is a Web Development Front-end Quizz so that you can t
 var z = 0;
 
 
-$("#welcome").fadeIn(200, function() {
+$("#welcome").fadeIn(2000, function() {
 
 	var typeWriter = setInterval(function() {
 
@@ -12,16 +12,16 @@ $("#welcome").fadeIn(200, function() {
 		z++;
 		if(z > myWelcomeText.length - 1) {
 			clearInterval(typeWriter);
-			$("#start").fadeIn(400);
+			$("#start").fadeIn(2000);
 		}
-	},1);
+	},5);
 	
 });
 
 $("#start").click(function() {
 
-	$("#welcome").fadeOut(500);
-	$("#quizz").fadeIn(200, function() {
+	$("#welcome").fadeOut();
+	$("#quizz").fadeIn(3000, function() {
 
 	$(".questions").html(quizz[0].question);
 	$("#1").html(quizz[0].answers.a);
@@ -42,7 +42,7 @@ var quizz = [
 {
 	question: "How old are you?",
 	answers: {a: 31, b: 32,c: 33,d: 34},
-	rightAnswer: "b"
+	rightAnswer: "a"
 },
 
 {
@@ -92,7 +92,7 @@ $(".next").click(function() {
 
 	userAnswers.push(c); // add the answer of the user to the userAnswers array
 
-	$(".progressbar").animate({width:"+=" + 20 + "%"}, 700); // the advancement of the progress bar
+	$(".progressbar").animate({width:"+=" + ((1/quizz.length) * 100) + "%"}, 700); // the advancement of the progress bar
 
 			if(counterQuest < quizz.length) {
 			// the export of the question and answers from the quizz array to the html quizz form
@@ -107,20 +107,44 @@ $(".next").click(function() {
 			} else if(counterQuest === quizz.length) {
 
 			
-				$(".a, .b, .c, .d, label, .questions, .progbarcontainer").remove();
-				$(this).val("See My Results").css({"font-size": "30px", "padding": "25px 0"}).animate({width: "100%"}, 2500);
-				$(this).click(function() {
-					$("#quizz").fadeOut(2000);
-					$("#final").fadeIn(1000);
-				})
+				
+				// $(this).val("See My Results").css({"font-size": "30px", "padding": "25px 0"}).animate({width: "100%"}, 2500);
+				// $(this).click(function() {
+				// 	$("#quizz").fadeOut(2000);
+				// 	$("#final").fadeIn(1000);
+				// })
+				$(this).fadeOut(3000);
+				$(".progressbar").after("<h2 style='padding: 20px;font-size: 30px;text-align: center;'>100% Completed</h2>");
+				$(".a, .b, .c, .d, label, .questions").remove();
+				$(".seeresult").fadeIn(3200).css({"font-size": "30px", "padding": "25px 0"}).animate({width: "100%"}, 2500);
 			}
 	
  })
 
 // Quizz page end:
 
+function userScore() {
+
+var result = 0;
+
+	for(var j = 0; j < quizz.length; j++) {
+
+		if(userAnswers[j] === quizz[j].rightAnswer) {
+
+			result++;
+
+		}
+
+	}
+return result;
+}
 
 
+$(".seeresult").click(function() {
+	$("#quizz").fadeOut();
+	$("#final").fadeIn(2500);
+	$("#final p").html(userScore() + " Out Of " + quizz.length);
+})
 
 
 
